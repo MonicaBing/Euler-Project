9c7fc56c2eb75b67256f8723b729405231b93572
MonicaBing <47180942+MonicaBing@users.noreply.github.com>
2020-05-26 17:04:06 +0100

    Add files via upload

---
+++ 21-30/problem_21.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 16:18:02 2020
+
+@author: kathykiutang
+"""
+
+import math
+
+def function(n):
+
+    max_div = math.ceil(n/2)
+    
+    divisor = []
+    for i in range (1,max_div+1):
+        if n%i == 0:
+            divisor.append(i)
+    return sum(divisor)
+
+
+
+result = 0
+
+for i in range (1,10000):
+    result1 = function(i) # 
+    result2 = function(result1)
+    
+    if (i != result1) & (i == result2) & (result1 != 1): # i = 9
+        result += result1 + result2
+        print(result)
+
+
